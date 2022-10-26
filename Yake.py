d6827c57d0ac32fcaf0bd34069ab61b752784bee
thiennn013 <70104878+thiennn013@users.noreply.github.com>
2022-10-26 09:15:26 +0700

    Update Yake.py

--- Yake.py
+++ Yake.py
@@ -5,21 +5,21 @@ import pandas as pd
 import time
 start_time = time.time()
 
-
-
-os.chdir('Texts')
 word=[]
 diem=[]
 
-for file in list(glob.glob('*.txt')):
-    File = open(file) #open file
-    lines = File.read() #read all lines
-    text =lines
-    yake = Yake(lan="en")
-    yake_keyphrases = yake.extract_keywords(text)
-    for i in yake_keyphrases:
-        word.append(i[0])
-        diem.append(i[1])
+
+file=('C:/Users/thien/PycharmProjects/NLP/Texts/africa_benin_activities.html.txt')
+File = open(file) #open file
+text =File.read() #read all lines
+
+
+yake = Yake(lan="en")
+yake_keyphrases = yake.extract_keywords(text)
+for i in yake_keyphrases:
+    word.append(i[0])
+    diem.append(i[1])
+
 
 data={
     'word': pd.Series(word),
@@ -27,11 +27,9 @@ data={
 }
 
 df=pd.DataFrame(data)
-df.sort_values(['diem','word'], inplace=True ,ascending=False)
 df.to_csv('C:/Users/thien/PycharmProjects/NLP/outYake.csv',sep='\t', encoding='utf-8',index=False)
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
 
 
 
